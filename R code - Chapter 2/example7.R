b1586988eb74ce23139cfbaf078c0c0aaf4e012a
Yousri El Fattah <yousri@causalcomputing.com>
2023-10-16 19:05:30 -0700

    Delete R code - Chapter 2 directory
    deleting the R code of previous version of Chapter 2

--- R code - Chapter 2/example7.R
+++
@@ -1,9 +0,0 @@
-# Example 7. A random variable takes on the value X = 1 
-# if the outcome of rolling a die is strictly greater than 4 
-# and X= 0 otherwise. What is the probability distribution for X? 
-# Calculate E[X], Var(X).
-#
-# Bernoulli(p)
-p <- 1./3
-cat("E[X] = ", p, "\n")
-cat("Var(X) = ", p*(1-p), "\n")
